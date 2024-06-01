88779ef13ea51d9502f575af4bcbcc199713bbee
Cong Wang <52726356+supersupercong@users.noreply.github.com>
2024-06-01 19:52:49 +0800

    Delete UHDformer directory

--- UHDformer/basicsr/__init__.py
+++
@@ -1,9 +0,0 @@
-# https://github.com/xinntao/BasicSR
-# flake8: noqa
-from .archs import *
-from .data import *
-from .losses import *
-from .models import *
-from .test import *
-from .train import *
-from .utils import *
--- UHDformer/test.sh
+++
@@ -1,3 +0,0 @@
-python setup.py develop
-CUDA_VISIBLE_DEVICES=0 python inference_uhdformer.py
-#CUDA_VISIBLE_DEVICES=6,7 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4398 inference_femasr_psnr_ssim_ychannel.py --launcher pytorch
